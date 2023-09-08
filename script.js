86bcc2e975b7e7837f0a7c0a33860649230f687a
Siraj Ahmed <gymsiraj@gmail.com>
2023-09-08 05:07:50 +0500

    scsc

--- script.js
+++ script.js
@@ -119,4 +119,31 @@
 //     });
 
 
+let delSections = document.querySelectorAll(".delayed-section");
+
+delSections.forEach(section => {
+  
+  let imageAnim = gsap.to(section.querySelector("img"), {
+    y: "-100vh",
+    ease: "none",
+    paused: true
+  });
+  
+  let progressTo = gsap.quickTo(imageAnim, "progress", {ease: "power3", duration: parseFloat(section.dataset.scrub) || 0.1});
+  
+  gsap.to(section.querySelector(".innerContainer"), {
+    y: "100vh",
+    ease: "none",
+    scrollTrigger: {
+      scrub: true,
+      trigger: section,
+      start: "top bottom",
+      end: "bottom top",
+      onUpdate: self => progressTo(self.progress)
+    }
+  });
+
+});
+
+
 
