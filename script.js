8a27552a13cd1261636aeee47843d4fa97bae375
Siraj Ahmed <gymsiraj@gmail.com>
2023-09-13 22:37:55 +0500

    cdcx

--- script.js
+++ script.js
@@ -225,6 +225,16 @@ function rotate(e){
   });
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+
+  menuToggle.addEventListener('click', function () {
+      menu.classList.toggle('active');
+      menuToggle.classList.toggle('active');
+  });
+});
+
 
 
 
