dffe8a1c30c82af6c1e99f953353f43be38ddf55
Siraj Ahmed <gymsiraj@gmail.com>
2023-09-18 06:43:49 +0500

    xxss

--- script.js
+++ script.js
@@ -239,14 +239,6 @@ const accordionItemHeaders = document.querySelectorAll(".accordion-item-header")
 
 accordionItemHeaders.forEach(accordionItemHeader => {
    accordionItemHeader.addEventListener("click", event => {
-    
-     // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-    
-//     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-//     if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-//        currentlyActiveAccordionItemHeader.classList.toggle("active");
-//        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-//      }
 
      accordionItemHeader.classList.toggle("active");
      const accordionItemBody = accordionItemHeader.nextElementSibling;
@@ -260,7 +252,13 @@ accordionItemHeaders.forEach(accordionItemHeader => {
    });
 });
 
+let loader = document.querySelector('#preloader');
 
+window.addEventListener('load', vanish);
+
+function vanish(){
+  loader.classList.add('disppear');
+}
 
 
 
