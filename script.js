6ea62ed4bd79695c578cc3b4d03bd4d8b1281bc0
Siraj Ahmed <gymsiraj@gmail.com>
2023-09-22 07:28:10 +0500

    xxs

--- script.js
+++ script.js
@@ -294,5 +294,60 @@ month.onclick = () =>{
 
 
 
+//our videos
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel({
+//   	loop:true,
+//     margin:10,
+//     nav:true,
+// 	autoplay:true,
+//     autoplayTimeout:3000,
+//     autoplayHoverPause:true,
+//     center: true,
+//     navText: [
+// 	    "<i class='fa fa-angle-left'></i>",
+// 	    "<i class='fa fa-angle-right'></i>"
+// 	],
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:3
+//         }
+//     }
+//   });
+// });
+
+const carousel = document.getElementById('carousel');
+        const prevBtn = document.getElementById('prevBtn');
+        const nextBtn = document.getElementById('nextBtn');
+        const carouselItems = document.querySelectorAll('.carousel-item');
+
+        let currentIndex = 0;
+
+        // Function to update the carousel display
+        function updateCarousel() {
+            carousel.style.transform = `translateX(-${currentIndex * 300}px)`;
+        }
+
+        // Event listener for the "Next" button
+        nextBtn.addEventListener('click', () => {
+            currentIndex = (currentIndex + 1) % carouselItems.length;
+            updateCarousel();
+        });
+
+        // Event listener for the "Previous" button
+        prevBtn.addEventListener('click', () => {
+            currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+            updateCarousel();
+        });
+
+
+
+
 
 
